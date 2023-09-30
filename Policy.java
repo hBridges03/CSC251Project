2daa73b34f7e17883896d8ef227eed4967743a2f
Hayden Bridges <hbridge4@email.cpcc.edu>
2023-09-30 02:47:14 -0400

    Update Policy.java
    Added documentation comments.

--- Policy.java
+++ Policy.java
@@ -44,64 +44,156 @@ public class Policy
    }
    
    //setters for all fields
+   
+   /*
+      Sets the value for the policy number.
+      
+      @param number - policy number
+   */
    public void setPolicyNumber(int number) {
       policyNumber = number;   
    }
+   /*
+      Sets the value for the provider name.
+      
+      @param name - provider name
+   */
    public void setProviderName(String name) {
       providerName = name;
    }
+   /*
+      Sets the value for the first name.
+      
+      @param first - first name
+   */
    public void setFirstName(String first) {
       firstName = first;
    }
+   /*
+      Sets the value for the last name.
+      
+      @param last - last name
+   */
    public void setLastName(String last) {
       lastName = last;
    }
+   /*
+      Sets the value for the age.
+      
+      @param a - age
+   */
    public void setAge(int a) {
       age = a;
    }
+   /*
+      Sets the smoking status.
+      
+      @param smoke - smoking status 
+   */
    public void setSmokingStatus(boolean smoke) {
       smokingStatus = smoke;
    }
+   /*
+      Sets the value for height.
+      
+      @param h - height
+   */
    public void setHeight(int h) {
    height = h;
    }
+   /*
+      Sets the value for weight.
+      
+      @param w - weight
+   */
    public void setWeight(int w) {
    weight = w;
    }
    
+   
    //getters for all fields
+   /*
+      Returns the value for the policy number.
+      
+      @return policyNumber
+   */
    public int getPolicyNumber() {
    return policyNumber;
    }
+   /*
+      Returns the value for the provider name.
+      
+      @return providerName
+   */
    public String getProviderName() {
    return providerName;
    }
+   /*
+      Returns the value for the first name.
+      
+      @return firstName
+   */
    public String getFirstName() {
    return firstName;
    }
+   /*
+      Returns the value for the last name.
+      
+      @return lastName
+   */
    public String getLastName() {
    return lastName;
    }
+   /*
+      Returns the value for the age.
+      
+      @return age
+   */
    public int getAge() {
    return age;
    }
+   /*
+      Returns the smoking status.
+      
+      @return smokingStatus
+   */
    public boolean getSmokingStatus() {
    return smokingStatus;
    }
+   /*
+      Returns the height.
+      
+      @return height
+   */
    public int getHeight() {
    return height;
    }
+   /*
+      Returns the weight.
+      
+      @return weight
+   */
    public int getWeight() {
    return weight;
    }
    
-   //calculate the policyholder's BMI
+   
+   /*
+      Calculates the BMI by using the BMI formula. 
+      
+      @return bmi
+   */
    public double getBMI() {
    double bmi;
    bmi = ((weight * 703) / (height* height));
    return bmi;
    }
    
+    /*
+      Calculates the policy price based on BMI, smoking status, and age.
+      
+      @return price
+   */
    public double policyPrice() {
    double price = 600;
    double bmi = getBMI();
@@ -120,7 +212,9 @@ public class Policy
    return price;
    }
    
-   //print class information
+   /*
+      Prints and formats class information.
+   */
    public void print() {
    String smoker;
    double bmi = getBMI();
